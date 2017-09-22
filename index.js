0f15c30a251de4335d62232ca68e83c059814e90
Domnik Mack <domnikmack@gmail.com>
2017-09-22 15:17:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deleteFromObjectByKey (object, key) {
   return newObject;
 }
 
-function detructivelyDeleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
 }
