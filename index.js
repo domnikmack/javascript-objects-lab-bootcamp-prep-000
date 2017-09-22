69b41c1ed0891ba991651cc8e536fa587e879c3b
Domnik Mack <domnikmack@gmail.com>
2017-09-22 14:53:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {};
+
+updateObjectWithKeyAndValue(object, key, value) {
+  Object.assign({}, object, {key: value} );
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value;
+  return object;
+}
